efdaa112cbd36297f6a726ed5132540a56fb45a4
Howard <howardchung04@yahoo.com.hk>
2024-05-15 16:56:14 +0100

    Update Response.java
    Change to record

--- src/main/java/org/example/dungeonsanddebugerss/model/exception/Response.java
+++ src/main/java/org/example/dungeonsanddebugerss/model/exception/Response.java
@@ -1,13 +1,4 @@
 package org.example.dungeonsanddebugerss.model.exception;
 
-public class Response {
-    private String message;
-    private int statusCode;
-    private String url;
-
-    public Response(String message, int statusCode, String url) {
-        this.message = message;
-        this.statusCode = statusCode;
-        this.url = url;
-    }
+public record Response (String message, int statusCode, String url) {
 }
