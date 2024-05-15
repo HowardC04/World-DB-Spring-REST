e0ca4e6d1c2b46c42906631e90bb31fbeaf9aa94
Howard <howardchung04@yahoo.com.hk>
2024-05-15 16:34:19 +0100

    Update Response.java
    Added response constructor

--- src/main/java/org/example/dungeonsanddebugerss/model/exception/Response.java
+++ src/main/java/org/example/dungeonsanddebugerss/model/exception/Response.java
@@ -1,4 +1,13 @@
 package org.example.dungeonsanddebugerss.model.exception;
 
 public class Response {
+    private String message;
+    private int statusCode;
+    private String url;
+
+    public Response(String message, int statusCode, String url) {
+        this.message = message;
+        this.statusCode = statusCode;
+        this.url = url;
+    }
 }
