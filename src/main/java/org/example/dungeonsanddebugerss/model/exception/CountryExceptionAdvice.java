5a1ab965a6334d0eb61cff70c88395003d7c88b6
Howard <howardchung04@yahoo.com.hk>
2024-05-16 09:27:37 +0100

    Update CountryExceptionAdvice.java
    Removed bad refactor

--- src/main/java/org/example/dungeonsanddebugerss/model/exception/CountryExceptionAdvice.java
+++ src/main/java/org/example/dungeonsanddebugerss/model/exception/CountryExceptionAdvice.java
@@ -1,7 +1,7 @@
 package org.example.dungeonsanddebugerss.model.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.example.dungeonsanddebugerss.exception.Response;
+import org.example.dungeonsanddebugerss.model.exception.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,8 +13,8 @@ public class CountryExceptionAdvice {
 
     @ExceptionHandler(CountryNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<org.example.dungeonsanddebugerss.exception.Response> countryNotfoundHandler(CountryNotFoundException e, HttpServletRequest request) {
-        org.example.dungeonsanddebugerss.exception.Response response = new Response(request.getRequestURL().toString(), 400, e.getMessage());
+    public ResponseEntity<Response> countryNotfoundHandler(CountryNotFoundException e, HttpServletRequest request) {
+        Response response = new Response(request.getRequestURL().toString(), 400, e.getMessage());
         return ResponseEntity.badRequest().body(response);
     }
 
