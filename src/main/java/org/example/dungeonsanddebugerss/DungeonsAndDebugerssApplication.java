d8fd754297751534e0e0e930bceda3152edf99e3
Howard <howardchung04@yahoo.com.hk>
2024-05-16 16:03:12 +0100

    Removed runner and added some documentation

--- src/main/java/org/example/dungeonsanddebugerss/DungeonsAndDebugerssApplication.java
+++ src/main/java/org/example/dungeonsanddebugerss/DungeonsAndDebugerssApplication.java
@@ -1,13 +1,7 @@
 package org.example.dungeonsanddebugerss;
 
-import org.example.dungeonsanddebugerss.service.CityService;
-import org.example.dungeonsanddebugerss.service.CountryLanguageService;
-import org.example.dungeonsanddebugerss.service.CountryService;
-import org.example.dungeonsanddebugerss.service.WorldService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import java.util.logging.Logger;
 
@@ -20,19 +14,4 @@ public class DungeonsAndDebugerssApplication {
     public static void main(String[] args) {
         SpringApplication.run(DungeonsAndDebugerssApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner runner(WorldService worldService, CityService cityService, CountryService countryService, CountryLanguageService countrylanguageService) {
-        return args -> {
-//            logger.info("Countries with no head of state: " + worldService.findCountriesWithNoHeadOfState());
-//            logger.info("Percentage of population in the largest city at Netherlands: " + worldService.findPercentageOfPopulationInLargestCity("Netherlands"));
-//            logger.info("5 Smallest districts of the city Noord-Holland: " + worldService.find5SmallestDistrictsOfCity("Noord-Holland"));
-//            logger.info("The number of people who speaks the most popular official language of South Korea is: " + worldService.findCountOfMostPopularLanguage("South Korea"));
-//            logger.info("Country with the most cities: " + worldService.findCountryWithMostCity() + " with " + worldService.returnNumOfCities() + " cities");
-//            logger.info(cityService.getAllCities().toString());
-//            logger.info(countryService.getCountryByCode("ABW").toString());
-//            logger.info(countrylanguageService.getAllCountryLanguages().toString());
-        };
-    }
-
 }
