64663d1bb1bab81c3b51768cde57c402d87af4b3
Howard <howardchung04@yahoo.com.hk>
2024-05-16 11:55:18 +0100

    Added tests files

---
+++ src/test/java/org/example/dungeonsanddebugerss/controller/CountryLanguageControllerTests.java
@@ -0,0 +1,7 @@
+package org.example.dungeonsanddebugerss.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CountryLanguageControllerTests {
+}
