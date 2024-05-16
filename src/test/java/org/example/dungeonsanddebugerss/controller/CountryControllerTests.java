6aec99758e2f120295a49f9ece1ebdb4f0b3690a
“selam” <Selamkd@users.noreply.github.com>
2024-05-16 15:31:15 +0100

    CountryController tests completed(@get)

--- src/test/java/org/example/dungeonsanddebugerss/controller/CountryControllerTests.java
+++ src/test/java/org/example/dungeonsanddebugerss/controller/CountryControllerTests.java
@@ -1,6 +1,7 @@
 package org.example.dungeonsanddebugerss.controller;
 
 import org.example.dungeonsanddebugerss.controllers.CountryController;
+import org.example.dungeonsanddebugerss.model.entities.CountryEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,32 +9,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@EnableWebMvc
 public class CountryControllerTests {
     private WebTestClient webTestClient;
-
+    @Autowired
     private CountryController countryController;
 
     @BeforeEach
-
     public void setup(){
-        webTestClient = WebTestClient.bindToServer().baseUrl("http://localhost:8000").build();
+        webTestClient = WebTestClient.bindToServer().baseUrl("http://localhost:8080").build();
     }
 
     @Test
-    @DisplayName("Check /countries  returns status code 200")
+    @DisplayName("Check endpoint /countries returns status code 200")
     @Transactional
-    void checkGetAuthorsReturns200(){
-        webTestClient.get().uri("/countries")
+    void checkGetCountriesReturns200(){
+        webTestClient.get().uri("/api/countries")
                 .exchange()
                 .expectStatus()
                 .is2xxSuccessful();
    }
-//
-//    @Test
-//    @DisplayName("Check ")
-
+    @Test
+    @DisplayName("Check that the first country is Afghanistan")
+    void checkThatFirstCountryIsAfgahanistan(){
+        webTestClient.get()
+                .uri("http://localhost:8080/api/country/AFG")
+                .exchange()
+                .expectBody(CountryEntity.class)
+                .value(country -> assertEquals("Afghanistan", country.getName()));
+    }
 
 
 }
